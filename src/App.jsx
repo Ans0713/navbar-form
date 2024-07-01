40f8093f6c2e0185d7bf6dda26c6a35c28e265d8
Ans0713 <138771207+Ans0713@users.noreply.github.com>
2024-07-01 16:27:20 +0530

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React, { useState } from 'react'
-import Navbar from './Component/Navbar/Navbar'
-import Home from './Component/Home/Home';
+import React, { useState } from 'react'; 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Component/Navbar/Navbar'; 
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
 
 const App = () => {
   const [theme, setTheme] = useState('light');
 
   return (
-    <div className='container'>
-      <Home theme={theme} setTheme={setTheme}/>
-    </div>
-  )
-}
+    <Router>
+      <Navbar theme={theme} setTheme={setTheme} username="" /> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
+
--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,35 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 import search_icon_light from '../../assets/search-w.png';
 import search_icon_dark from '../../assets/search-b.png';
 import toggle_icon_light from '../../assets/day.png';
 import toggle_icon_dark from '../../assets/night.png';
 
-const Navbar = ({username,theme,setTheme}) => {
-    const toggle_mode = ()=> {
-        theme=== "light"? setTheme('dark') : setTheme('light');
-    }
+const Navbar = ({ username = 'User', theme, setTheme }) => {
+  const toggle_mode = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
   return (
     <>
-   
-    <div style={theme=== "light"?{backgroundColor:"lightcoral"}:{backgroundColor:"rgb(118, 33, 33)"}} className='navbar'>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Login</li>
-        <li>Navbar</li>
-      </ul>
+      <div className={`navbar ${theme}`}>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
 
-      <div className='search-box'>
-        <input type="text" placeholder='Search'/>
-        <img src={search_icon_light} alt="Search Icon" />
+        <div className="search-box">
+          <input type="text" placeholder="Search" />
+          <img src={theme === 'light' ? search_icon_light : search_icon_dark} alt="Search Icon" />
+        </div>
+
+        <img
+          onClick={toggle_mode}
+          src={theme === 'light' ? toggle_icon_light : toggle_icon_dark}
+          alt="Toggle Icon"
+          className="toggle-icon"
+        />
       </div>
-      
-
-      <img onClick={()=>{toggle_mode()}} src={theme=== "light"? toggle_icon_light : toggle_icon_dark} alt="Toggle Icon" className='toggle-icon'/>
-    </div>
-    <div className="hello">hello, How are you {username}</div>
+      <div className="hello">Hello, How are you {username}?</div>
     </>
   );
-}
-export default Navbar; 
+};
+
+export default Navbar;
